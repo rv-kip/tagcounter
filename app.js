73595f4982594a684555e84ea4d78411bef4278b
Kip Gebhardt <kgebhardt@kqed.org>
2015-08-16 17:32:32 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+var express                 = require('express'),
+    logger                  = require('./lib/logger'),
+    request                 = require('request'),
+    jsdom                   = require('jsdom'),
+    html                    = require("html"),
+    config                  = require('./config/config.js').config;
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('port', config.server.port || 8081);
+app.set('hostname', config.server.hostname || '0.0.0.0');
+app.use('/public', express.static('public'));
+
+// Routes
+app.get('*', function (req, res, next){ // Log all requests
+    var app = req.app;
+    var logline = ['method=' + req.method, 'path=' + req.path, 'ip=' + req.ip];
+    logger.info(logline.join(' '));
+    next();
+});
+app.get("/", handle_index);
+app.get("/lookup", handle_count);
+
+// Handlers
+function handle_index(req, res) {
+    var jade_elements = {
+            title       : "Tag Counter",
+        };
+    return res.render('index', jade_elements);
+}
+
+function handle_count(req, res){
+    url = req.query.url || null;
+
+    // ensure http://
+    if (url && url.match(/^http/) === null) {
+        url = 'http://' + url;
+    }
+    logger.info('url', url);
+
+    request.get(url, function (err, response, body){
+        if (err) {
+            return res.send("Error: " + err.message);
+        }
+
+        // Convert HTML to DOM, get tag counts, sort
+        var scripts = ["http://code.jquery.com/jquery.js"];
+        jsdom.env( body, scripts, function(err, window){
+            var document = window.document;
+            var $ = window.jQuery;
+            var tagcount = {};
+            $('*').each(function(index) {
+                var tagname = $( this ).prop("tagName").toLowerCase() ||  null;
+                logger.info('tagName', tagname, index);
+                if (tagcount[tagname]) {
+                    tagcount[tagname] ++;
+                } else {
+                    tagcount[tagname] = 1;
+                }
+            });
+
+            // sort tagcount by count
+            var tagcount_arr = [];
+            for (var tag in tagcount) {
+                tagcount_arr.push([tag, tagcount[tag]]);
+            }
+            tagcount_arr.sort(function(a, b){
+                return b[1] - a[1];
+            });
+
+            // Pretty print format the source
+            var ret_body = safe_tags(html.prettyPrint(window.document.documentElement.outerHTML, {indent_size: 2}));
+
+            var jade_elements = {
+                title       : "Tag Counter",
+                page_source : ret_body,
+                summary     : tagcount_arr
+            };
+
+            return res.render('index', jade_elements);
+        });
+    });
+}
+
+// utilities
+function safe_tags(str) {
+    return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') ;
+}
+
+// Start server
+var server = app.listen(app.get('port'), app.get('hostname'), function() {
+    logger.info('Server listening on http://' + app.get('hostname') + ':' + app.get('port'));
+});
